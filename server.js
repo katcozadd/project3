f2f117e40c73fa25cbaa8d26938d6fbf93f8e60e
katcozadd <krosecozadd@gmail.com>
2018-05-14 16:35:29 -0600

    cors

--- server.js
+++ server.js
@@ -6,7 +6,15 @@ const sgMail     = require('@sendgrid/mail');
 const cors       = require('cors')
 require('dotenv').config();
 
+app.use(cors());
+app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+  next();
+});
 
 app.post('/contact', function(req, res) {
 	console.log(req.body);
