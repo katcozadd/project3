d23625c5d66474c731707d6164fde0bc01eb9a92
katcozadd <krosecozadd@gmail.com>
2018-05-14 16:07:18 -0600

    deploying to heroku

---
+++ server.js
@@ -0,0 +1,31 @@
+const express    = require('express');
+const app        = express();
+const path       = require('path');
+const bodyParser = require('body-parser');
+const sgMail     = require('@sendgrid/mail');
+require('dotenv').config();
+
+
+
+app.post('/contact', function(req, res) {
+	console.log(req.body);
+			sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+			console.log(process.env.SENDGRID_API_KEY);
+				const msg = {
+				  to: `${req.body.email}`,
+				  from: 'hello@acafeboise.com',
+				  subject: 'New contact submission from acafeboise.com: ' `${req.body.subject}`,
+				  text: `${req.body.message}`,
+				  html: `${req.body.message}`,
+				};
+				sgMail.send(msg);
+				res.json(msg);
+	});
+
+
+// listen on port 3000
+  app.set('port', process.env.PORT || 3001)
+
+  app.listen(app.get('port'), () => {
+    console.log(`✅ PORT: ${app.get('port')} 🌟`)
+  })
